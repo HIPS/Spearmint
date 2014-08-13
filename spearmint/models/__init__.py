938a36c6c70ea006f911b53503e732547668c8cf
Jasper Snoek <jaspersnoek@gmail.com>
2014-08-13 17:24:29 -0400

    Clean up

--- spearmint/models/__init__.py
+++ spearmint/models/__init__.py
@@ -1,6 +1,4 @@
 from gp            import GP
 from gp_classifier import GPClassifier
-from ***REMOVED***          import ***REMOVED***
-from mixed_gp      import MixedGP
 
-__all__ = ["GP", "GPClassifier", "***REMOVED***", 'MixedGP']
+__all__ = ["GP", "GPClassifier"]
